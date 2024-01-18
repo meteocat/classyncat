9e6809c1fae33c2c7527ba2e6ce0fee62bf01146
JR Miró <jr,miro@gencat.cat>
2024-01-18 13:17:05 +0100

    RAM-802 Changes in dowload ERA5 data and add config file

--- classificacio_sinoptica.py
+++ classificacio_sinoptica.py
@@ -5,28 +5,36 @@ from utils.centre_graella import calcula_centre_graella
 from classyncat.jenkinson_collison_sfc import jenkinson_collison_sfc
 from classyncat.jenkinson_collison_500 import jenkinson_collison_500
 from classyncat.classyncat import classyncat
+from utils.load_config import load_config
 
 import numpy as np
 
 if __name__ == '__main__':
     
     np.seterr(divide='ignore')
-    INPUT1 = './data/mslp_00.grib'
-    INPUT2 = './data/500mb_00.grib'
-    PUNTS1 = './data/list_points_mslp'
-    PUNTS2 = './data/list_points_500mb'
 
-    INPUT_DIR = './data/ERA5/'
     DATE_INI = "2022-02-01"
     DATE_FI = "2022-05-13"
-    # Calculem la latitud central que despres s'usara per J&C
-    # download_ERA5(date_ini=DATE_INI, date_fi=DATE_FI, 
-    #                input_dir=INPUT_DIR)
+
+    # Read the configuration file
+    config = load_config('config_classyncat.json')
+    PUNTS1 = config["mslp_grid_points"]
+    PUNTS2 = config["500mb_grid_points"]
+    INPUT_DIR = config["input_dir"]
+    
+    # Download the data from ERA5
+    download_ERA5(date_ini=DATE_INI, date_fi=DATE_FI,
+                  input_dir=INPUT_DIR)
+    # Calculate the center latitude of the grid for further calculations
     CENLAT = calcula_centre_graella(PUNTS1)
+    # Read the grib generated from the data downloaded
     GRID_SLP, GRID_500 = llegeix_grib_punts(input_dir=INPUT_DIR, 
                                             punts1=PUNTS1, punts2=PUNTS2)
+    # Calculate the Jenkinson & Collison for sfc
     TIPUS_SFC = jenkinson_collison_sfc(GRID_SLP, CENLAT)
+    # Calculate the types for 500 mb
     TIPUS_500 = jenkinson_collison_500(GRID_500, CENLAT)
+    # Merge both classifications for obtaining a new one
     CLASSIFICACIO = classyncat(TIPUS_SFC, TIPUS_500)
 
     DATE_INI = DATE_INI.replace("-", "")
--- inout/download_era5.py
+++ inout/download_era5.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python
 import cdsapi
-import pandas as pd
 import os
 import glob
 
@@ -21,9 +20,10 @@ def download_ERA5(date_ini: dt.datetime,
     """
 
     # Delete the old data
-    files = glob.glob(input_dir)
-    for f in files:
-        os.remove(f)
+    files = glob.glob(input_dir+'/*')
+    if files != []:
+        for f in files:
+            os.remove(f)
 
     # We change to the directory where data will be put
     os.chdir(input_dir)
@@ -31,65 +31,21 @@ def download_ERA5(date_ini: dt.datetime,
     # Call the api client
     c = cdsapi.Client()
 
-    # To optimize the download from Copernicus we must separate
-    # the time in months.
-    list_dates = pd.date_range(date_ini, date_fi, 
-                               freq='BMS').strftime("%Y-%m-%d").tolist()
-    # If it's less than a month
-    if len(list_dates) == 1:
-        [date1, date2] = [date_ini, date_fi]
-        if (date1 != date2):
-            c.retrieve('reanalysis-era5-single-levels', {
-                'product_type': 'reanalysis',
-                'area': [50, -20, 30, 10],  # North, West, South, East
-                'format': 'grib',
-                'date': date1 + '/' + date2,
-                'variable': 'mean_sea_level_pressure',
-                'time': '00:00',
-            }, "era5_daily_slp_%s.grb" % date1)
+    c.retrieve('reanalysis-era5-single-levels', {
+        'product_type': 'reanalysis',
+        'area': [50, -20, 30, 10],  # North, West, South, East
+        'format': 'grib',
+        'date': date_ini + '/' + date_fi,
+        'variable': 'mean_sea_level_pressure',
+        'time': '00:00',
+    }, "era5_slp_%s_%s.grb" % (date_ini, date_fi))
 
-            c.retrieve('reanalysis-era5-pressure-levels', {
-                'area': [50, -20, 30, 10],  # North, West, South, East.
-                'product_type': 'reanalysis',
-                'format': 'grib',
-                'variable': 'geopotential',
-                'pressure_level': '500',
-                'date': date1 + '/' + date2,
-                'time': '00:00',
-            }, "era5_daily_500_%s.grb" % (date1))
-    # More than one month of data
-    else:
-        for idx in range(len(list_dates)):
-            if (idx == 0):
-                if (dt.datetime.strptime(list_dates[0], "%Y-%m-%d") >
-                   dt.datetime.strptime(date_ini, "%Y-%m-%d")):
-                    [date1, date2] = [date_ini, list_dates[1]]
-                else:
-                    [date1, date2] = [date_ini, list_dates[1]]
-            elif (idx < len(list_dates)-1):
-                [date1, date2] = [list_dates[idx], list_dates[idx+1]]
-            elif (dt.datetime.strptime(list_dates[-1], "%Y-%m-%d") <
-                  dt.datetime.strptime(date_fi, "%Y-%m-%d")):
-                [date1, date2] = [list_dates[-1], date_fi]
-            else:
-                [date1, date2] = [list_dates[idx], date_fi]
-
-            if (date1 != date2):
-                c.retrieve('reanalysis-era5-single-levels', {
-                    'product_type': 'reanalysis',
-                    'area': [50, -20, 30, 10],  # North, West, South, East
-                    'format': 'grib',
-                    'date': date1 + '/' + date2,
-                    'variable': 'mean_sea_level_pressure',
-                    'time': '00:00',
-                }, "era5_daily_slp_%s.grb" % date1)
-
-                c.retrieve('reanalysis-era5-pressure-levels', {
-                    'area': [50, -20, 30, 10],  # North, West, South, East.
-                    'product_type': 'reanalysis',
-                    'format': 'grib',
-                    'variable': 'geopotential',
-                    'pressure_level': '500',
-                    'date': date1 + '/' + date2,
-                    'time': '00:00',
-                }, "era5_daily_500_%s.grb" % (date1))
+    c.retrieve('reanalysis-era5-pressure-levels', {
+        'area': [50, -20, 30, 10],  # North, West, South, East.
+        'product_type': 'reanalysis',
+        'format': 'grib',
+        'variable': 'geopotential',
+        'pressure_level': '500',
+        'date': date_ini + '/' + date_fi,
+        'time': '00:00',
+    }, "era5_500_%s_%s.grb" % (date_ini, date_fi))
