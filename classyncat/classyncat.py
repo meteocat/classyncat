2fbdc2d1bc50b434cc53326ab28c43f8be7c8d0b
ecasellas <enric.casellas@gmail.com>
2024-01-23 11:32:28 +0100

    Refactor Classyncat classification

--- classyncat/classyncat.py
+++ classyncat/classyncat.py
@@ -1,149 +1,144 @@
-def classyncat(tipus_sfc: dict, tipus_500: dict) -> dict:
-    """This subroutine is the main core of the synoptic classification in which
-    is based in:
-    Miró et al(2020): Daily atmospheric circulation patterns for Catalonia
-    (northeast Iberian Peninsula) using a modified version of Jenkinson and
-    Collison method.
-    Respect the original we've made some changes:
-    TIP11 (Dana in the SW) was assimilated to TIP09 (SW advection)
-    TIP13 (Thermal Low) and TIP14 (Barometric swamp) were fussioned.
-    TIP16 (Anticiclo Termic) ha estat assimilat al anticiclo normal (TIP13)    
+"""Module to implement Classyncat circulation type classification from
+
+Miró et al(2020): Daily atmospheric circulation patterns for Catalonia
+                  (northeast Iberian Peninsula) using a modified version of
+                  Jenkinson and Collison method.
+"""
+
+
+def classyncat(jc_sfc: str, jc_500: str) -> str:
+    """Synoptic circulation classification based on Miró et al., (2020) "Daily
+    atmospheric circulation patterns for Catalonia (northeast Iberian Peninsula)
+    using a modified version of Jenkinson and Collison method.
 
     Types:
-    TIP01: Adveccio Oest
-    TIP02: Adveccio del Oest Anticiclonica
-    TIP03: Adveccio del NW
-    TIP04: Adveccio del N
-    TIP05: Adveccio del NE
-    TIP06: Adveccio del E
-    TIP07: Adveccio del E amb gota freda
-    TIP08: Adveccio del S
-    TIP09: Adveccio del SW
-    TIP10: Solc
-    TIP11: Ciclo
-    TIP12: Indefinit o baixa superficial
-    TIP13: Anticiclo
+        - TIP01: Advection W
+        - TIP02: Advection W with Anticyclone
+        - TIP03: Advection NW
+        - TIP04: Advection N
+        - TIP05: Advection NE
+        - TIP06: Advection E
+        - TIP07: Advection E with a cut-off low
+        - TIP08: Advection S
+        - TIP09: Advection SW
+        - TIP10: Trough
+        - TIP11: Cyclone
+        - TIP12: Undetermined or surface low
+        - TIP13: Anticyclone
+
+    Changes to Miró et al., (2020):
+        - TIP11 (DANA in the SW) assimilated to TIP09 (SW Advection).
+        - TIP13 (Thermal Low) and TIP14 (Barometric swamp) fusioned.
+        - TIP16 (Thermal Anticylone) assimilated to TIP13 (Anticyclone).
 
     Args:
-        tipus_sfc (dict): Corresponding types calcultad with surface pressure
-        tipus_500 (dict): Corresponding types calcultad with 500mb geopotential
+        jc_sfc (str): Jenkinson-Collison surface circulation type.
+        jc_500 (str): Jenkinson-Collison 500 hPa circulation type.
 
     Returns:
-        dict: classificacio obtinguda amb la combinació de les dues 
-        classificacions.
+        str: Classyncat circulation type.
     """
+    cc_type = "None"
 
-    tipus = 'None'
-    classificacio = {}
+    # Slicing for the types in sfc and 500 we construct the new
+    # classification
+    if jc_sfc == "A":
+        if jc_500[0] == "C":
+            if jc_500 == "C":
+                cc_type = "TIP13"
+            elif jc_500 in ["CSW", "CW", "CNW"]:
+                cc_type = "TIP02"
+            else:
+                cc_type = "TIP13"
+        elif jc_500[0] == "A":
+            if jc_500 == "A":
+                cc_type = "TIP13"
+            elif jc_500 in ["ASW", "AW", "ANW"]:
+                cc_type = "TIP02"
+            else:
+                cc_type = "TIP13"
+        else:
+            if jc_500 in ["SW", "W", "NW"]:
+                cc_type = "TIP02"
+            else:
+                cc_type = "TIP13"
 
-    for dia in tipus_sfc.keys():
-        tipus1 = tipus_sfc[dia]
-        tipus2 = tipus_500[dia]
-        # Slicing for the types in sfc and 500 we construct the new
-        # classification
-        if (tipus1 == 'A'):
-            if (tipus2[0] == 'C'):
-                if (tipus2 == 'C'):
-                    tipus = 'TIP13'
-                elif (tipus2 == 'CSW' or tipus2 == 'CW' or tipus2 == 'CNW'):
-                    tipus = 'TIP02'
-                else:
-                    tipus = 'TIP13'
-            elif (tipus2[0] == 'A'):
-                if (tipus2 == 'A'):
-                    tipus = 'TIP13'
-                elif (tipus2 == 'ASW' or tipus2 == 'AW' or tipus2 == 'ANW'):
-                    tipus = 'TIP02'
-                else:
-                    tipus = 'TIP13'
+    elif jc_sfc in ["AE", "ASE"]:
+        if jc_500[0] == "C":
+            cc_type = "TIP07"
+        else:
+            cc_type = "TIP06"
+    elif jc_sfc == "AS":
+        cc_type = "TIP08"
+    elif jc_sfc == "ASW":
+        cc_type = "TIP09"
+    elif jc_sfc == "AW":
+        cc_type = "TIP02"
+    elif jc_sfc == "ANW":
+        cc_type = "TIP03"
+    elif jc_sfc == "AN":
+        cc_type = "TIP04"
+    elif jc_sfc == "ANE":
+        cc_type = "TIP05"
+    elif jc_sfc == "C":
+        if jc_500[0] == "C":
+            cc_type = "TIP11"
+        elif jc_500[0] == "A":
+            if jc_500 in ["AW", "ASW", "AE", "ASE", "A"]:
+                cc_type = "TIP12"
             else:
-                if (tipus2 == 'SW' or tipus2 == 'W' or tipus2 == 'NW'):
-                    tipus = 'TIP02'
-                else:
-                    tipus = 'TIP13'
+                cc_type = "TIP11"
+        elif jc_500 == "U":
+            cc_type = "TIP12"
+        else:
+            if jc_500 in ["W", "SW", "E", "SE"]:
+                cc_type = "TIP12"
+            else:
+                cc_type = "TIP11"
+    elif jc_sfc in ["CE", "CSE"]:
+        if jc_500[0] == "C":
+            cc_type = "TIP07"
+        else:
+            cc_type = "TIP06"
+    elif jc_sfc == "CS":
+        cc_type = "TIP08"
+    elif jc_sfc == "CSW":
+        cc_type = "TIP09"
+    elif jc_sfc == "CW":
+        if jc_500[0] == "C":
+            cc_type = "TIP10"
+        else:
+            cc_type = "TIP01"
+    elif jc_sfc == "CNW":
+        cc_type = "TIP03"
+    elif jc_sfc == "CN":
+        cc_type = "TIP04"
+    elif jc_sfc == "CNE":
+        cc_type = "TIP05"
+    elif jc_sfc in ["E", "SE"]:
+        if jc_500[0] == "C":
+            cc_type = "TIP07"
+        else:
+            cc_type = "TIP06"
+    elif jc_sfc == "S":
+        cc_type = "TIP08"
+    elif jc_sfc == "SW":
+        cc_type = "TIP09"
+    elif jc_sfc == "W":
+        cc_type = "TIP01"
+    elif jc_sfc == "NW":
+        cc_type = "TIP03"
+    elif jc_sfc == "N":
+        cc_type = "TIP04"
+    elif jc_sfc == "NE":
+        cc_type = "TIP05"
+    elif jc_sfc == "U":
+        if jc_500[0] == "C":
+            if jc_500 in ["CS", "CSW", "CSE"]:
+                cc_type = "TIP09"
+            else:
+                cc_type = "TIP10"
+        else:
+            cc_type = "TIP12"
 
-        elif (tipus1 == 'AE' or tipus1 == 'ASE'):
-            if (tipus2[0] == 'C'):
-                tipus = 'TIP07'
-            else:
-                tipus = 'TIP06'
-        elif (tipus1 == 'AS'):
-            tipus = 'TIP08'
-        elif (tipus1 == 'ASW'):
-            tipus = 'TIP09'
-        elif (tipus1 == 'AW'):
-            tipus = 'TIP02'
-        elif (tipus1 == 'ANW'):
-            tipus = 'TIP03'
-        elif (tipus1 == 'AN'):
-            tipus = 'TIP04'
-        elif (tipus1 == 'ANE'):
-            tipus = 'TIP05'
-
-        elif (tipus1 == 'C'):
-            if (tipus2[0] == 'C'):
-                tipus = 'TIP11'
-            elif (tipus2[0] == 'A'):
-                if (tipus2 == 'AW' or tipus2 == 'ASW' or tipus2 == 'AE' or
-                   tipus2 == 'ASE' or tipus2 == "A"):
-                    tipus = 'TIP12'
-                else:
-                    tipus = 'TIP11'
-            elif (tipus2 == 'U'):
-                tipus = 'TIP12'
-            else:
-                if (tipus2 == 'W' or tipus2 == 'SW' or
-                   tipus2 == 'E' or tipus2 == 'SE'):
-                    tipus = 'TIP12'
-                else:
-                    tipus = 'TIP11'
-
-        elif (tipus1 == 'CE' or tipus1 == 'CSE'):
-            if (tipus2[0] == 'C'):
-                tipus = 'TIP07'
-            else:
-                tipus = 'TIP06'
-        elif (tipus1 == 'CS'):
-            tipus = 'TIP08'
-        elif (tipus1 == 'CSW'):
-            tipus = 'TIP09'
-        elif (tipus1 == 'CW'):
-            if (tipus2[0] == 'C'):
-                tipus = 'TIP10'
-            else:
-                tipus = 'TIP01'
-        elif (tipus1 == 'CNW'):
-            tipus = 'TIP03'
-        elif (tipus1 == 'CN'):
-            tipus = 'TIP04'
-        elif (tipus1 == 'CNE'):
-            tipus = 'TIP05'
-        elif (tipus1 == 'E' or tipus1 == 'SE'):
-            if (tipus2[0] == 'C'):
-                tipus = 'TIP07'
-            else:
-                tipus = 'TIP06'
-        elif (tipus1 == 'S'):
-            tipus = 'TIP08'
-        elif (tipus1 == 'SW'):
-            tipus = 'TIP09'
-        elif (tipus1 == 'W'):
-            tipus = 'TIP01'
-        elif (tipus1 == 'NW'):
-            tipus = 'TIP03'
-        elif (tipus1 == 'N'):
-            tipus = 'TIP04'
-        elif (tipus1 == 'NE'):
-            tipus = 'TIP05'
-        elif (tipus1 == 'U'):
-            if (tipus2[0] == 'C'):
-                if (tipus2 == 'CS' or tipus2 == 'CSW' or tipus2 == 'CSE'):
-                    tipus = 'TIP09'
-                else:
-                    tipus = 'TIP10'
-            else:
-                tipus = 'TIP12'
-
-        classificacio[dia] = tipus
-
-    return classificacio
+    return cc_type
