1e8670f3652403fe6224a531619abcbbf8776d68
ecasellas <enric.casellas@gmail.com>
2024-02-09 08:26:32 +0100

    Update utils grid error

--- tests/test_utils_grid.py
+++ tests/test_utils_grid.py
@@ -1,5 +1,6 @@
 """Module to test utils.grid
 """
+
 import unittest
 
 from classyncat.utils.grid import get_grid_from_file
@@ -7,11 +8,20 @@ from classyncat.utils.grid import get_grid_from_file
 
 class TestUtilsGrid(unittest.TestCase):
     """Class to test utils grid module"""
+
     def test_get_grid_from_file(self):
         """Test get grid center and extension from file points"""
-        grid_points = [[45.0, -10.0], [45.0, 0.0], [45.0, 10.0],
-                       [40.0, -10.0], [40.0, 0.0], [40.0, 10.0],
-                       [35.0, -10.0], [35.0, 0.0], [35.0, 10.0]]
+        grid_points = [
+            [45.0, -10.0],
+            [45.0, 0.0],
+            [45.0, 10.0],
+            [40.0, -10.0],
+            [40.0, 0.0],
+            [40.0, 10.0],
+            [35.0, -10.0],
+            [35.0, 0.0],
+            [35.0, 10.0],
+        ]
 
         center, grid_extension = get_grid_from_file(grid_points)
 
@@ -20,3 +30,26 @@ class TestUtilsGrid(unittest.TestCase):
         self.assertEqual(grid_extension[1], -10.0)
         self.assertEqual(grid_extension[2], 35.0)
         self.assertEqual(grid_extension[3], 10.0)
+
+    def test_get_grid_from_file_wrong_format(self):
+        """Test get grid center and extension from file points"""
+        grid_points = [
+            [45.0],
+            [45.0, 0.0],
+            [45.0, 10.0],
+            [40.0, -10.0],
+            [40.0, 0.0],
+            [40.0, 10.0],
+            [35.0, -10.0],
+            [35.0, 0.0],
+            [35.0, 10.0],
+        ]
+
+        with self.assertRaises(IndexError) as err:
+            get_grid_from_file(grid_points)
+
+        self.assertEqual(
+            err.exception.args[0],
+            "`grid_points` points may not be properly configured. They "
+            "must be a list of latitude and longitude coordinate tuples.",
+        )
