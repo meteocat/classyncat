c49f3d76145d578072b7b8487ebbb7dbde679e13
ecasellas <enric.casellas@gmail.com>
2024-02-09 08:30:26 +0100

    Complete test io grib points

--- tests/test_io_grib_points.py
+++ tests/test_io_grib_points.py
@@ -1,5 +1,6 @@
 """Module to test IO grib points.
 """
+
 import unittest
 
 from classyncat.io.grib_points import get_jc_grib_points
@@ -8,22 +9,38 @@ from classyncat.io.grib_points import get_jc_grib_points
 class TestIOGribPoints(unittest.TestCase):
     """Class to test IO grib points functions"""
 
+    grib_mslp = "tests/data/era5_slp_2022-02-01.grb"
+    grib_z500 = "tests/data/era5_500_2022-02-01.grb"
+
+    points_mslp = [
+        [45.0, -10.0],
+        [45.0, 0.0],
+        [45.0, 10.0],
+        [40.0, -10.0],
+        [40.0, 0.0],
+        [40.0, 10.0],
+        [35.0, -10.0],
+        [35.0, 0.0],
+        [35.0, 10.0],
+    ]
+
+    points_z500 = [
+        [45.0, -15.0],
+        [45.0, -5.0],
+        [45.0, 5.0],
+        [40.0, -15.0],
+        [40.0, -5.0],
+        [40.0, 5.0],
+        [35.0, -15.0],
+        [35.0, -5.0],
+        [35.0, 5.0],
+    ]
+
     def test_get_jc_grib_points(self):
         """Test extract grib value for specific points"""
 
-        grib_mslp = "tests/data/era5_slp_2022-02-01.grb"
-        grib_z500 = "tests/data/era5_500_2022-02-01.grb"
-
-        points_mslp = [[45.0, -10.0], [45.0, 0.0], [45.0, 10.0],
-                       [40.0, -10.0], [40.0, 0.0], [40.0, 10.0],
-                       [35.0, -10.0], [35.0, 0.0], [35.0, 10.0]]
-
-        points_z500 = [[45.0, -15.0], [45.0, -5.0], [45.0, 5.0],
-                       [40.0, -15.0], [40.0, -5.0], [40.0, 5.0],
-                       [35.0, -15.0], [35.0, -5.0], [35.0, 5.0]]
-
         grib_mslp, grib_z500 = get_jc_grib_points(
-            grib_mslp, points_mslp, grib_z500, points_z500
+            self.grib_mslp, self.points_mslp, self.grib_z500, self.points_z500
         )
 
         self.assertEqual(
@@ -53,4 +70,22 @@ class TestIOGribPoints(unittest.TestCase):
                 56445.8359375,
                 55412.8359375,
             ],
+        )
+
+    def test_get_jc_grib_points_wrong_type(self):
+        """Test correct type for grib data/file"""
+        with self.assertRaises(ValueError) as err:
+            get_jc_grib_points([], self.points_mslp, self.grib_z500, self.points_z500)
+
+        self.assertEqual(
+            err.exception.args[0],
+            "`grib_mslp` must be the string path to a grib file or and xarray.Dataset.",
+        )
+
+        with self.assertRaises(ValueError) as err:
+            get_jc_grib_points(self.grib_mslp, self.points_mslp, [], self.points_z500)
+
+        self.assertEqual(
+            err.exception.args[0],
+            "`grib_z500` must be the string path to a grib file or and xarray.Dataset.",
         )
